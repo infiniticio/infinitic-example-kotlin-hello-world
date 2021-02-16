a0ac69b020739adc9c268c32b0b0441c31420586
Gilles Barbier <gilles@zenaton.com>
2021-02-16 15:49:24 +0100

    prepare 0.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,17 +13,15 @@ plugins {
 }
 
 repositories {
-    mavenLocal()
     mavenCentral()
     jcenter()
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.+")
     implementation("org.slf4j:slf4j-simple:1.7.+")
 
-    implementation("io.infinitic:infinitic-pulsar:0.2.1-SNAPSHOT")
-    implementation("io.infinitic:infinitic-client:0.2.1-SNAPSHOT")
+    implementation("io.infinitic:infinitic-pulsar:0.2.1")
+    implementation("io.infinitic:infinitic-client:0.2.1")
 }
 
 application {
